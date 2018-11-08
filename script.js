7ee56a3c5bd8272da25454b7803b8f8e7082ac89
WahiMac10 <wahimac10@WahiMac10s-MacBook-Air.local>
2018-11-08 15:59:15 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let color =["Blue ", "Green ", "Red ", "Orange ", "Violet ", "Indigo", "Yellow"];
+
+let o = [ "th", "st", "nd", "rd"];
+
+   for (let i=0; i < color.length; i++){
+		
+		if (i ===0){ 
+			document.getElementById("colorOrder").innerHTML += i + 1 + o[1] + " choice is " + color[i]+ "<hr/>";
+
+		}
+		else if (i ===1){ 
+			document.getElementById("colorOrder").innerHTML += i + 1 + o[2] + " choice is " + color[i] + "<hr/>";
+		}
+		else if (i ===2){ 
+			document.getElementById("colorOrder").innerHTML += i + 1 + o[3] + " choice is " + color[i]+ "<hr/>";
+		}
+		else { 
+			document.getElementById("colorOrder").innerHTML += i + 1 + o[0] + " choice is " + color[i]+ "<hr/>";
+		}
+	} 
+
+
+   
+
